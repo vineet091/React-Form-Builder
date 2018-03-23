7b516225b8b6231c844cef72740db478ba8a1bb5
Vineet Gupta <vineet091@users.noreply.github.com>
2018-03-23 12:23:36 +0530

    Update Select.js

--- src/js/components/FormFields/Select.js
+++ src/js/components/FormFields/Select.js
@@ -20,15 +20,16 @@ class Select extends React.Component {
     }
 
     onChange = (event) => {
-        const { name } = this.props;
-        if (this.props.onChange && event && event.target) {
-            this.props.onChange(name, event.target.value);
+        const { name, onChange } = this.props;
+        if (onChange && event && event.target) {
+            onChange(name, event.target.value);
         }
     }
 
     onBlur = (event) => {
-        if (this.props.onBlur) {
-            this.props.onBlur(event.target.value);
+        const { onBlur } = this.props;
+        if typeof onBlur === 'function') {
+            onBlur(event.target.value);
         }
     }
 
